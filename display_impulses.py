1aeb60794e5af53be6645d9acf03ee5867482def
greg-lee-surf <gregpaulsimon@gmail.com>
2022-12-19 10:44:59 +0100

    Retrait du 'enable' car inutile.

--- display_impulses.py
+++ display_impulses.py
@@ -6,23 +6,22 @@ from py_aff3ct.module.py_module import Py_Module
 
 class display_impulses(Py_Module):
 
-	def display(self, x, y, BE, enable):#heat_map et h_ix sont des constantes
-		if enable!=0:
-			#self.map[self.H-self.ytempo-1,self.xtempo] = np.array([BE*self.heat_map[self.h_ix,0], BE*self.heat_map[self.h_ix,1], BE*self.heat_map[self.h_ix,2]])
-			
-			self.map[self.H-y-1,x,0] = BE*self.heat_map[self.h_ix,0]
-			self.map[self.H-y-1,x,1] = BE*self.heat_map[self.h_ix,1]
-			self.map[self.H-y-1,x,2] = BE*self.heat_map[self.h_ix,2]
-			
-			
-			if self.xtempo==0:
-				self.xtempo = 159
-				if self.ytempo <159:
-					self.ytempo+=1
-				else:
-					self.toggle_done()
+	def display(self, x, y, BE):#heat_map et h_ix sont des constantes
+		#self.map[self.H-self.ytempo-1,self.xtempo] = np.array([BE*self.heat_map[self.h_ix,0], BE*self.heat_map[self.h_ix,1], BE*self.heat_map[self.h_ix,2]])
+		
+		self.map[self.H-y-1,x,0] = BE*self.heat_map[self.h_ix,0]
+		self.map[self.H-y-1,x,1] = BE*self.heat_map[self.h_ix,1]
+		self.map[self.H-y-1,x,2] = BE*self.heat_map[self.h_ix,2]
+		
+		
+		if self.xtempo==0:
+			self.xtempo = 159
+			if self.ytempo <159:
+				self.ytempo+=1
 			else:
-				self.xtempo-=1
+				self.toggle_done()
+		else:
+			self.xtempo-=1
 			
 		return 0
     
@@ -33,7 +32,7 @@ class display_impulses(Py_Module):
 		self.name 		= "display_impulses"   # Set your module's name
 		self.xtempo 	= 0
 		self.ytempo 	= 0
-		self.heat_map = heat_map
+		self.heat_map   = heat_map
 		self.h_ix	 	= h_ix
 		self.H 			= H
 		self.W 			= W
@@ -44,6 +43,5 @@ class display_impulses(Py_Module):
 		s_x 	 = self.create_socket_in  (t_dis, "x"   	, 1, np.int32   ) # create an input socket for the task t_dis
 		s_y		 = self.create_socket_in  (t_dis, "y"   	, 1, np.int32   ) # create an input socket for the task t_dis
 		s_BE	 = self.create_socket_in  (t_dis, "BE"   	, 1, np.int32   ) # create an input socket for the task t_dis
-		s_enable = self.create_socket_in (t_dis, "enable"  , 1, np.int32   ) # create an input socket for the task t_dis
 		
-		self.create_codelet(t_dis, lambda slf, lsk, fid: slf.display(lsk[s_x], lsk[s_y], lsk[s_BE], lsk[s_enable])) # create codelet
+		self.create_codelet(t_dis, lambda slf, lsk, fid: slf.display(lsk[s_x], lsk[s_y], lsk[s_BE])) # create codelet
